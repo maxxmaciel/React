61d264fcbb8c415758d733b91a314d6e327a8cf2
maxxmaciel <88160151+maxxmaciel@users.noreply.github.com>
2022-04-29 10:20:40 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+import { Header } from './componentes/header.js';
+
+
+function App() {
+  return (
+    <div className="main-container">
+    <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/header.js
@@ -0,0 +1,28 @@
+
+
+export function Header() {
+  return (
+
+<div className="header">
+        <div id="nav-1">
+          <input type="text" name="name" placeholder="Pesquisar..." />
+          <div >
+      
+          </div>
+
+          <div id="logos">
+          
+          </div>
+        </div>
+        <div id="nav-2">
+          <ul>
+            <a>Home</a>
+            <a>Serviços </a>
+            <a>Quem Somos</a>
+            <a>Contato</a>  
+            </ul>  
+        </div>
+</div>
+
+ );
+}
